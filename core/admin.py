438cb0f175f61d6e250aad8bde148a418d869085
BiancheTI <alceubianchetfilho@gmail.com>
2024-08-30 21:44:57 -0300

    Lançamento Django

--- core/admin.py
+++ core/admin.py
@@ -1,8 +1,15 @@
 from django.contrib import admin
-
-from .models import Categoria, Conta
+from django.contrib.admin import AdminSite
+from .models import Categoria, Conta, LancamentoFinanceiro
 
 # Register your models here.
+admin.site.site_header = "Projeto Finanças"
+admin.site.site_title = "Projeto Finanças"
+admin.site.site_url = "Bem Vindo ao Projeto Finanças"
+
+class LancamentoAdmin (admin.ModelAdmin):
+    list_display = ('data', 'historico', 'credito_debito', 'valor')
 
 admin.site.register(Categoria)
 admin.site.register(Conta)
+admin.site.register(LancamentoFinanceiro, LancamentoAdmin)
